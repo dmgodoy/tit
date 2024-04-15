d1ee9b41f46fca6e59ff75b12ee43c723a58c47c
David Medina <davidmedinagodoy@gmail.com>
2024-04-15 15:29:37 +0200

    add and update files in index

--- tit-add.sh
+++ tit-add.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
-
-hash_cmd="git hash-object"
+git-hash(){
+    cat $1 | git hash-object --stdin
+}
+in-index(){
+    egrep -q "^$1 " $2
+}
+hash_cmd="git-hash"
 INDEX=".tit/index"
 if [ "$1" = "" ]; then
     echo "Usage tit add <path-to-file>"
+    exit 1
+fi
+if [ ! -d .tit ]; then
+    echo "Not a tit repository"
+    exit 1
 fi
 
 touch $INDEX
 for f in $@; do
-    if [ ! -f $f ] && ! grep -qFx $f .tit/index; then
+    #file does not exist and it is not in the index
+    if [ ! -f $f ] && ! in-index $f $INDEX; then
 	   echo "$f does not exists"
-    fi ;
-    if ! grep -qFx $f $INDEX; then
-	   echo $f >> $INDEX 
-    fi ;
+           exit 1
+    fi
+    #file exists but it is not in the index
+    if ! in-index $f $INDEX; then
+        hash=$($hash_cmd $f)
+        echo $f $hash >> $INDEX 
+        echo "Added $f $hash to $INDEX"
+        exit 0
+    fi
+    #flie exists and it is already in the index
+    if [ -f $f ] && in-index $f $INDEX; then
+        hash=$($hash_cmd $f)
+        old_hash=$(egrep "^$f " $INDEX | awk '{print $2}')
+        if [ "$old_hash" = "$hash" ]; then
+            echo "Nothing to do for $f"
+            exit 0
+        else
+            sed -i "s/^$f .*$/$f $hash/g" $INDEX
+            echo "Updated file $f in index: $old_hash => $hash"
+            exit 0
+        fi
+    fi 
     
 done
